f1192c1ecf30fcc1594207ff8c62ccde3498de77
Nodirbek <66748319+Nodirbek-Abdulaxadov@users.noreply.github.com>
2024-02-15 20:46:30 +0500

    Update Constants.cs

--- Messager/Models/Constants.cs
+++ Messager/Models/Constants.cs
@@ -4,6 +4,4 @@ internal static class Constants
     public static string BASE_URL = "https://notify.eskiz.uz/api/";
     public static string LOGIN_URL = BASE_URL + "auth/login";
     public static string Send_SMS_URL = BASE_URL + "message/sms/send";
-    public static string EMAIL = "nodirbekabdulaxadov2000@gmail.com";
-    public static string SecretKey = "iBgj5LmVwdEGT46YHXkMxA6MFFjFKzbieWWEvbgh";
 }
